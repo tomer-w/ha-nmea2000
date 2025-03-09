88f2371c2c0493ae4b4c4e5d68b8555115e1a24c
Tomer <57483589+tomer-w@users.noreply.github.com>
2025-03-09 21:47:09 +0200

    ruff fixes

--- custom_components/nmea2000/SerialSensor.py
+++ custom_components/nmea2000/SerialSensor.py
@@ -2,11 +2,11 @@ import asyncio
 import serial_asyncio
 from serial import SerialException
 import binascii
-from homeassistant.components.sensor import  SensorEntity
+from homeassistant.components.sensor import SensorEntity
 from homeassistant.core import callback
-from .sensorCommon import set_pgn_entity
 
 import logging
+
 _LOGGER = logging.getLogger(__name__)
 
 DEFAULT_NAME = "Serial Sensor"
@@ -18,12 +18,12 @@ DEFAULT_XONXOFF = False
 DEFAULT_RTSCTS = False
 DEFAULT_DSRDTR = False
 
+
 # SerialSensor class representing a sensor entity interacting with a serial device
 class SerialSensor(SensorEntity):
     """Representation of a Serial sensor."""
 
     _attr_should_poll = False
-    
 
     def __init__(
         self,
@@ -44,16 +44,13 @@ class SerialSensor(SensorEntity):
         self._dsrdtr = DEFAULT_DSRDTR
         self._serial_loop_task = None
         self._attributes = None
-        
+
         self._retry_delay = 5  # Reconnection tart with 5 seconds
         self._max_delay = 60  # Reconnection maximum delay of 1 minutes
-
-
 
     async def async_added_to_hass(self) -> None:
         """Handle when an entity is about to be added to Home Assistant."""
         self._serial_loop_task = self.hass.loop.create_task(self.serial_read())
-
 
     async def read_loop(self, reader):
         """Continuously read data from the serial port."""
@@ -70,35 +67,35 @@ class SerialSensor(SensorEntity):
                         buffer = bytearray()
                     break
                 buffer.extend(data)
-    
+
                 # Continue processing as long as there's data in the buffer
                 while True:
                     # Find the packet start and end delimiters
-                    start = buffer.find(b'\xaa')
-                    end = buffer.find(b'\x55', start)
-                    
+                    start = buffer.find(b"\xaa")
+                    end = buffer.find(b"\x55", start)
+
                     if start == -1 or end == -1:
                         # If start or end not found, wait for more data
                         break
-    
+
                     # Extract the complete packet, including the end delimiter
-                    packet = buffer[start:end+1]
-    
+                    packet = buffer[start : end + 1]
+
                     # Process the packet
-                    if len(packet) > 2:  # Make sure it's not just the header and end code
+                    if (
+                        len(packet) > 2
+                    ):  # Make sure it's not just the header and end code
                         self.process_packet(packet)
-    
+
                     # Remove the processed packet from the buffer
-                    buffer = buffer[end+1:]
-    
+                    buffer = buffer[end + 1 :]
+
         except Exception as exc:
             _LOGGER.exception("Error while reading from serial port: %s", exc)
         finally:
             _LOGGER.debug("Finished reading data")
 
-
     async def serial_read(self):
-        
         """Read the data from the port."""
         while True:
             try:
@@ -112,24 +109,29 @@ class SerialSensor(SensorEntity):
                     rtscts=self._rtscts,
                     dsrdtr=self._dsrdtr,
                 )
-                
+
                 _LOGGER.debug("Serial connection established")
                 await self.read_loop(reader)
-                
-                
+
             except SerialException as exc:
-                _LOGGER.exception("Serial connection failed: %s. Retrying in %d seconds...", exc, self._retry_delay)
+                _LOGGER.exception(
+                    "Serial connection failed: %s. Retrying in %d seconds...",
+                    exc,
+                    self._retry_delay,
+                )
                 await self._handle_error()
             except asyncio.CancelledError:
                 _LOGGER.debug("Serial read task was cancelled")
                 break
             except Exception as exc:
-                _LOGGER.exception("Unexpected error: %s. Retrying in %d seconds...", exc, self._retry_delay)
+                _LOGGER.exception(
+                    "Unexpected error: %s. Retrying in %d seconds...",
+                    exc,
+                    self._retry_delay,
+                )
                 await self._handle_error()
 
-
     def process_packet(self, packet):
-        
         if len(packet) < 7:  # AA + E8 + Frame ID (4 bytes min) + 55
             _LOGGER.error("Invalid packet length: %s", binascii.hexlify(packet))
             return
@@ -140,39 +142,42 @@ class SerialSensor(SensorEntity):
         # Extract the type byte and data length from the type byte
         type_byte = packet[1]
         data_length = type_byte & 0x0F  # last 4 bits represent the data length
-        
+
         # Extract and reverse the frame ID
         frame_id = packet[2:6][::-1]
-        
+
         # Convert frame_id bytes to an integer
-        frame_id_int = int.from_bytes(frame_id, byteorder='big')
-        
+        frame_id_int = int.from_bytes(frame_id, byteorder="big")
+
         # Extracting Source ID from the frame ID
         source_id = frame_id_int & 0xFF
-        source_id_hex = '{:02X}'.format(source_id)
-        
+        source_id_hex = "{:02X}".format(source_id)
+
         # Extracting PGN ID from the frame ID
-        pgn_id = (frame_id_int >> 8) & 0x3FFFF  # Shift right by 8 bits and mask to 18 bits
-        pgn_id_hex = '{:06X}'.format(pgn_id)  # Format PGN as a hex string with 6 digits
-        
+        pgn_id = (
+            frame_id_int >> 8
+        ) & 0x3FFFF  # Shift right by 8 bits and mask to 18 bits
+        pgn_id_hex = "{:06X}".format(pgn_id)  # Format PGN as a hex string with 6 digits
+
         # Extract and reverse the CAN data
-        can_data = packet[6:6 + data_length][::-1]
-        can_data_hex = binascii.hexlify(can_data).decode('ascii')
-        
+        can_data = packet[6 : 6 + data_length][::-1]
+        can_data_hex = binascii.hexlify(can_data).decode("ascii")
+
         # Prepare combined string in the format "PGN:Source_ID:CAN_Data"
         combined_hex = f"{pgn_id_hex}:{source_id_hex}:{can_data_hex}"
-        
+
         # Log the extracted information including the combined string
-        _LOGGER.debug("USB %s PGN ID: %s, Frame ID: %s, CAN Data: %s, Source ID: %s, Combined: %s",
+        _LOGGER.debug(
+            "USB %s PGN ID: %s, Frame ID: %s, CAN Data: %s, Source ID: %s, Combined: %s",
             self.name,
             pgn_id_hex,
-            binascii.hexlify(frame_id).decode('ascii'),
+            binascii.hexlify(frame_id).decode("ascii"),
             can_data_hex,
             source_id_hex,
-            combined_hex)
-        
+            combined_hex,
+        )
+
         set_pgn_entity(self.hass, self.name, combined_hex)
-        
 
     async def _handle_error(self):
         """Handle error for serial connection."""
@@ -181,7 +186,9 @@ class SerialSensor(SensorEntity):
         self.async_write_ha_state()
         await asyncio.sleep(5)
         await asyncio.sleep(self._retry_delay)
-        self._retry_delay = min(self._retry_delay * 2, self._max_delay)  # Double the delay, up to a maximum
+        self._retry_delay = min(
+            self._retry_delay * 2, self._max_delay
+        )  # Double the delay, up to a maximum
 
     @callback
     def stop(self, event):
